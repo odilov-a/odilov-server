f696438656c50af96e0e510a1bbe596cfc83c355
odilov-a <akbarjonodilov01@gmail.com>
2024-02-22 16:23:00 +0500

    #

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -5,7 +5,7 @@ const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 router.get("/", blogController.getAllBlogs);
 router.get("/:id", blogController.getBlogById);
-// router.use(authMiddleware.authenticateToken);
+router.use(authMiddleware.authenticateToken);
 router.post("/", blogController.createBlog);
 router.put("/:id", blogController.updateBlog);
 router.delete("/:id", blogController.deleteBlog);
